9c59938020e982d22e975c84a9367a0f34ad6154
Hellano0209 <hellano.vda@gmail.com>
2024-05-20 20:55:00 -0300

    atualização: texto inicial currículo

--- codigo_python/curriculo_portifolio.py
+++ codigo_python/curriculo_portifolio.py
@@ -7,10 +7,22 @@ Created on Thu May 16 21:43:22 2024
 """
 
 import streamlit as st
-from PIL import Image
 
 st.header('Hellano Vieira de Almeida')
 
-# Open the image from the specified path
-image = Image.open('~/Documentos/Projetos Python/Currículo e Portifólio/curriculo-streamlit/imagens/foto_perfil.jpg')
-st.image(image)
+# dados para contato
+st.sidebar.link_button('Linkedin', 'https://www.linkedin.com/in/hellano-vieira-de-almeida-b4987359/') 
+st.sidebar.write('e-mail: hellano.vda@gmail.com')
+st.sidebar.write('telefone: (85) 99688-6866')
+
+st.text(
+    
+    '''
+    Sou Bacharel em estatística e mestre em Economia pela universidade Federal do \n
+    Ceará (UFC). Tenho experiência análise de dados (análise descritiva, elaboração \n
+    de gráficos e tabelas, análise inferencial, teste de hipóteses, construção de \n
+    modelos de regressão e séries temporais). Tenho conhecimentos em linguagem de \n
+    programação (Python, R, Git/GitHub).
+    '''
+    
+    )
