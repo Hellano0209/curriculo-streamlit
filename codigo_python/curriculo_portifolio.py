76c78229bed2ca00b49d106cb77247a39ebf62bd
Hellano0209 <hellano.vda@gmail.com>
2024-06-01 22:02:57 -0300

    adicionei abas referentes a resumos geral, experiencia proficional e portifólio. adicionei algumas experiencias proficionais

--- codigo_python/curriculo_portifolio.py
+++ codigo_python/curriculo_portifolio.py
@@ -16,45 +16,98 @@ st.sidebar.write('telefone: (85) 99688-6866')
 
 st.header('Hellano Vieira de Almeida', divider=True)
 
-st.subheader('Resumo')
-with st.container(border = True):
-    st.text(
+def inform_geral():
+    
+    #### RESUMO GERAL
+    st.subheader('Resumo')
+    
+    with st.container(border = True):
+        st.text(
+            
+            '''
+            Sou Bacharel em Estatística e Mestre em Economia pela Universidade Federal do
+            Ceará (UFC). Tenho experiência em análise de dados: análise descritiva de 
+            dados (elaboração de gráficos e tabelas, dashboards), análise estatística 
+            inferencial (testes de hipóteses, estimação de modelos estatísticos), construção 
+            de modelos estatísticos e machine learning para previsão (modelos de regressão 
+            linear e logística, modelos de séries temporais). Tenho bons conhecimentos em 
+            linguagem de programação (Python, R, Git/GitHub).
+            '''
+            
+            )
+
+    st.divider()
+    #### FIM RESUMO GERAL
+    
+    #### FORMAÇÃO ACADEMICA
+    st.subheader('Formação Acadêmica')
+    with st.container(border = True):
+        st.markdown(
+            
+            '''
+            * Bachalelado em Estatítica: Universidade Federal do Ceará
+            * Mestrado em Economia: Universidade Federal do Ceará
+            '''
+            
+            )
         
+    st.divider()
+    #### FIM FORMAÇÃO ACADEMICA
+    
+    #### CURSOS DE APERFEIÇOAMENTO
+    st.subheader('Cursos de Aperfeiçoamento')
+    with st.container(border = True):
+        st.markdown(
+            '''
+            * Fundamentos de Linguagem Python para Análise de Dados e Data Science - Nível Intermediário
+            * Microsoft Power BI Para Business Intelligence e Data Science
+            * Análise de Viabilidade Econômica e Financeira de Projetos
+            * Versionamento de Código com Git e GitHub
+            '''
+            
+            )
+    
+    #### FIM CURSOS DE APERFEIÇOAMENTO
+    
+def exp_prof():
+    
+    #### SESA
+    st.markdown(
         '''
-        Sou Bacharel em Estatística e Mestre em Economia pela Universidade Federal do
-        Ceará (UFC). Tenho experiência em análise de dados como: análise descritiva de 
-        dados (elaboração de gráficos e tabelas, dashboards), análise estatística 
-        inferencial (testes de hipóteses, estimação de modelos estatísticos), construção 
-        de modelos estatísticos e machine learning para previsão (modelos de regressão 
-        linear e logística, modelos de séries temporais). Tenho bons conhecimentos em 
-        linguagem de programação (Python, R, Git/GitHub).
+        ### Secretaria da Saúde do Estado do Ceará (SESA)
+        #### Analista de Gestão (Dados)
+        jul/2023-atual
         '''
-        
+        )
+    
+    with st.container(border = True):
+        st.markdown(
+            '''
+            **Atividades realizadas:**
+            * reunir informações de várias fontes, como registros médicos, dados de pacientes, pesquisas e estudos clínicos;
+            * limpar e organizar os dados, garantindo que estejam prontos para análise;
+            * utilizar técnicas estatísticas/econométricas/ML, para explorar os dados identificando padrões, tendências e correlações epidemiológicas;
+            * traduzir os resultados da análise em gráficos e tabelas;
+            * desenvolver modelos preditivos que auxiliem na previsão de demandas assistenciais.
+            
+            **Ferramentas utilizadas:** Excel, Google sheet, Software R, python, Power BI
+            '''
+            )
+    st.divider()
+    ####
+    
+    #### Cearáprev
+    st.markdown(
+        '''
+        ### Cearáprev
+        #### Analista de Dados
+        jan/2022-abr/2023
+        '''
         )
 
-st.divider()
-    
-st.subheader('Formação Acadêmica')
-with st.container(border = True):
-    st.markdown(
-        
-        '''
-        * Bachalelado em Estatítica: Universidade Federal do Ceará
-        * Mestrado em Economia: Universidade Federal do Ceará
-        '''
-        
-        )
-    
-st.divider()
+tab1, tab2, tab3 = st.tabs(['Resumo Geral', 'Experiência Proficional', 'Portifólio'])
 
-st.subheader('Cursos de Aperfeiçoamento')
-with st.container(border = True):
-    st.markdown(
-        '''
-        * Fundamentos de Linguagem Python para Análise de Dados e Data Science - Nível Intermediário
-        * Microsoft Power BI Para Business Intelligence e Data Science
-        * Análise de Viabilidade Econômica e Financeira de Projetos
-        * Versionamento de Código com Git e GitHub
-        '''
-        
-        )
+with tab1:
+    inform_geral()
+with tab2:
+    exp_prof()
